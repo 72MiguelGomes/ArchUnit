28625d97b2916fe13007252591697f75393b4bd1
Miguel Gomes <jmbg.41@gmail.com>
2018-11-11 08:03:35 +0000

    Create dependencies

--- src/main/java/com/example/arch/controller/Controller.java
+++ src/main/java/com/example/arch/controller/Controller.java
@@ -1,5 +1,12 @@
 package com.example.arch.controller;
 
+import com.example.arch.core.Core;
+
 public class Controller {
 
+  private final Core core;
+
+  public Controller(Core core) {
+    this.core = core;
+  }
 }
--- src/main/java/com/example/arch/core/Core.java
+++ src/main/java/com/example/arch/core/Core.java
@@ -1,5 +1,12 @@
 package com.example.arch.core;
 
+import com.example.arch.repo.Repository;
+
 public class Core {
 
+  private final Repository repository;
+
+  public Core(Repository repository) {
+    this.repository = repository;
+  }
 }
